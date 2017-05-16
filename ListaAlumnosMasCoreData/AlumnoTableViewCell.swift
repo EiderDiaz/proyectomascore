d9f21367bb2f5e6249b671708df162927b213dd2
eiderdiaz <eiderdiaz@eiderdiazs-Mac.local>
2017-05-16 16:38:13 -0600

    fist comit

---
+++ ListaAlumnosMasCoreData/AlumnoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlumnoTableViewCell.swift
+//  ListaAlumnosMasCoreData
+//
+//  Created by eiderdiaz on 5/15/17.
+//  Copyright © 2017 eiderdiaz. All rights reserved.
+//
+
+import UIKit
+
+class AlumnoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ListaAlumnosMasCoreData/ProtocoloAlumno.swift
@@ -0,0 +1,9 @@
+//
+//  ProtocoloAlumno.swift
+//  ListaAlumnosMasCoreData
+//
+//  Created by eiderdiaz on 5/15/17.
+//  Copyright © 2017 eiderdiaz. All rights reserved.
+//
+
+import Foundation
